111f7daaf4a9ff2de7bd19e84312b69e3cc7a11f
s1mpson <s1mpson@yahoo.com>
2020-05-30 20:01:19 +0300

    :tada: Adding mod.ts

---
+++ mod.ts
@@ -0,0 +1,4 @@
+export { default as extractEmoji } from "./lib/extract.ts";
+export { default as hasEmoji } from "./lib/has.ts";
+export { default as isEmoji } from "./lib/is.ts";
+export { default as trimEmoji } from "./lib/trim.ts";
